d1f07a3ff53d2085d8680d2bcc1c84dc38f0e194
xinyi-jin <1747402195@qq.com>
2021-02-09 17:15:03 +0800

    978. 最长湍流子数组（动态规划和双指针解法）

--- leetcode/alg978/maxTurbulenceSize.go
+++ leetcode/alg978/maxTurbulenceSize.go
@@ -17,7 +17,7 @@ import (
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
 
 // 本题采用官方题解
-func maxTurbulenceSize(arr []int) int {
+/* func maxTurbulenceSize(arr []int) int {
 	n := len(arr)
 	ans := 1
 	left, right := 0, 0
@@ -46,7 +46,74 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+} */
+
+// 思路 假若从0~5 是湍流数组，那0~5最大的就是湍流数组就是0~5，下次开始查找子数组就是从6开始查找
+// 动态规划实现
+/* func maxTurbulenceSize(arr []int) int {
+	n := len(arr)
+	if n < 2 {
+		return 1
+	}
+
+	// arr[k] < arr[k+1]结尾
+	increased := make([]int, n)
+	// arr[k] > arr[k+1]结尾
+	decreased := make([]int, n)
+
+	increased[0] = 1
+	decreased[0] = 1
+	res := 1
+	for i := 1; i < n; i++ {
+		if arr[i] > arr[i-1] {
+			increased[i] = decreased[i-1] + 1
+			decreased[i] = 1
+		} else if arr[i] < arr[i-1] {
+			decreased[i] = increased[i-1] + 1
+			increased[i] = 1
+		} else {
+			increased[i] = 1
+			decreased[i] = 1
+		}
+		res = max(res, max(increased[i], decreased[i]))
+	}
+	return res
+} */
+
+// 滑动窗口实现(双指针)
+func maxTurbulenceSize(arr []int) int {
+	n := len(arr)
+	if n < 2 {
+		return 1
+	}
+
+	flag := false //true 大于 false 小于
+	res := 1
+	left, right := 0, 1
+	for right < n {
+		f := arr[right-1] < arr[right]
+
+		if right == 1 || f == flag {
+			left = right - 1
+		}
+
+		if arr[right-1] == arr[right] {
+			left = right
+		}
+		right++
+		res = max(right-left, res)
+		flag = f
+	}
+	return res
 }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	arr := make([]int, 10)
 	arr = []int{9, 4, 2, 10, 7, 8, 8, 1, 9}
