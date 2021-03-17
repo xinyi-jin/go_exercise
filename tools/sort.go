18d9f0d4ee38af1723c4e90ad641f64cdb72ece4
xinyi-jin <iezhuhe@163.com>
2021-03-17 18:01:45 +0800

    自定义int64数组排序

---
+++ tools/sort.go
@@ -0,0 +1,10 @@
+package tools
+
+import "sort"
+
+type Int64Slice []int64
+
+func (p Int64Slice) Len() int           { return len(p) }
+func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p Int64Slice) Sort()              { sort.Sort(p) }
