4d087158fe403d4c4415d04175caf79d6c8a50b9
xinyi-jin <iezhuhe@163.com>
2021-04-30 12:15:07 +0800

    优化算法

--- leetcode/slot/line.go
+++ leetcode/slot/line.go
@@ -24,7 +24,36 @@ func IsLine(data []int) (e int, count int) {
 	return -1, -1
 }
 
+// 处理单个结果
 func IsLineEx(data []int) (e int, count int) {
+	tempData := make([]int, 5)
+	copy(tempData, data)
+	sort.Ints(tempData)
+
+	curPos := 0
+	curElement := tempData[0]
+
+	for i := 1; i < len(tempData); i++ {
+		n := i - curPos
+		if curElement == tempData[len(tempData)-1] && len(tempData)-curPos >= 3 {
+			return curElement, len(tempData) - curPos
+		}
+		if n >= 3 {
+			if curElement != tempData[i] {
+				return curElement, n
+			}
+		} else {
+			if curElement != tempData[i] {
+				curElement = tempData[i]
+				curPos = i
+			}
+		}
+	}
+	return -1, -1
+}
+
+// 可处理多个结果
+func IsLineExtend(data []int, flag bool) (e int, count int) {
 	tempData := make([]int, 5)
 	copy(tempData, data)
 	sort.Ints(tempData)
@@ -34,14 +63,25 @@ func IsLineEx(data []int) (e int, count int) {
 	arrResult := make([]int64, 0)
 	for i := 1; i < len(tempData); i++ {
 		n := i - curPos
-		if n >= 3 {
-			if curElement != tempData[i] && len(tempData)-i < 3 {
-				return curElement, n
-			} else if curElement != tempData[i] && len(tempData)-i >= 3 {
-				curElement = tempData[i]
-				curPos = i
+		if curElement == tempData[len(tempData)-1] && len(tempData)-curPos >= 3 {
+			if flag {
 				calcValue := int64(curElement) + int64(n)*10000
 				arrResult = append(arrResult, calcValue)
+				break
+			}
+			return curElement, len(tempData) - curPos
+		}
+		if n >= 3 {
+			if curElement != tempData[i] {
+				if flag {
+					calcValue := int64(curElement) + int64(n)*10000
+					arrResult = append(arrResult, calcValue)
+
+					curElement = tempData[i]
+					curPos = i
+					continue
+				}
+				return curElement, n
 			}
 		} else {
 			if curElement != tempData[i] {
@@ -49,9 +89,8 @@ func IsLineEx(data []int) (e int, count int) {
 				curPos = i
 			}
 		}
-
 	}
-	if len(arrResult) > 0 {
+	if flag && len(arrResult) > 0 {
 		// 处理多个结果
 	}
 	return -1, -1
--- test/slot_test.go
+++ test/slot_test.go
@@ -13,3 +13,50 @@ func TestIsLine(t *testing.T) {
 	e, n = slot.IsLineEx(data)
 	fmt.Println(e, n)
 }
+
+func TestIsLineEx(t *testing.T) {
+	type TestData struct {
+		data []int
+		line int
+	}
+	testData := []TestData{
+		{data: []int{0, 0, 0, 1, 1}, line: 3},
+		{data: []int{1, 0, 0, 0, 1}, line: 3},
+		{data: []int{1, 1, 0, 0, 0}, line: 3},
+
+		{data: []int{0, 1, 0, 0, 1}, line: 3},
+		{data: []int{0, 1, 1, 0, 0}, line: 3},
+
+		{data: []int{0, 0, 1, 0, 1}, line: 3},
+		{data: []int{0, 0, 1, 1, 0}, line: 3},
+
+		{data: []int{0, 0, 0, 0, 1}, line: 4},
+		{data: []int{1, 0, 0, 0, 0}, line: 4},
+		{data: []int{0, 1, 0, 0, 0}, line: 4},
+		{data: []int{0, 0, 1, 0, 0}, line: 4},
+		{data: []int{0, 0, 0, 1, 0}, line: 4},
+
+		{data: []int{0, 0, 0, 0, 0}, line: 5},
+	}
+	for _, value := range testData {
+		if _, count := slot.IsLine(value.data); count != value.line {
+			t.Error(slot.IsLine(value.data))
+			t.Error("Error line data:", value)
+			t.Fatal("TestIsLine")
+		}
+	}
+	errorData := []TestData{
+		{data: []int{1, 2, 0, 0, 1}, line: -1},
+		{data: []int{1, 2, 2, 0, 1}, line: -1},
+		{data: []int{1, 2, 2, 4, 1}, line: -1},
+		{data: []int{1, 2, 2, 1, 3}, line: -1},
+		{data: []int{1, 1, 0, 1, 1}, line: -1},
+	}
+	for _, value := range errorData {
+		if _, count := slot.IsLine(value.data); count != value.line {
+			t.Error(slot.IsLine(value.data))
+			t.Error("Error data:", value)
+			t.Fatal("TestIsLine")
+		}
+	}
+}
