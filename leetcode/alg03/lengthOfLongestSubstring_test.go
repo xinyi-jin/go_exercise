99dabebd11b1df3908ebf0df48ac3a8c98e73a92
jinxinyi <1747402195.com>
2021-10-16 18:18:39 +0800

    3. 无重复字符的最长子串（暂存）

---
+++ leetcode/alg03/lengthOfLongestSubstring_test.go
@@ -0,0 +1,28 @@
+package alg03
+
+import "testing"
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test1", args{"abcabcbb"}, 3},
+		{"Test2", args{"bbbbb"}, 1},
+		{"Test3", args{"pwwkew"}, 3},
+		{"Test4", args{""}, 0},
+		{"Test5", args{"aa"}, 1},
+		{"Test5", args{"au"}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
