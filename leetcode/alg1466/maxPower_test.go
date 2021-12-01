c5d291ec603cedb393a0610614feb9c9997462b5
jinxinyi <1747402195.com>
2021-12-01 16:38:17 +0800

    1446. 连续字符

---
+++ leetcode/alg1466/maxPower_test.go
@@ -0,0 +1,29 @@
+package alg1466
+
+import "testing"
+
+func Test_maxPower(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test_maxPower_1", args{"leetcode"}, 2},
+		{"Test_maxPower_2", args{"abbcccddddeeeeedcba"}, 5},
+		{"Test_maxPower_3", args{"triplepillooooow"}, 5},
+		{"Test_maxPower_4", args{"hooraaaaaaaaaaay"}, 11},
+		{"Test_maxPower_5", args{"tourist"}, 1},
+		{"Test_maxPower_6", args{"a"}, 1},
+		{"Test_maxPower_7", args{"cc"}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxPower(tt.args.s); got != tt.want {
+				t.Errorf("%q. maxPower() = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
