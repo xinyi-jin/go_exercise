b4cfadf935168d9a44ff56506f221f625714272a
jinxinyi <1747402195.com>
2022-03-18 16:38:26 +0800

    1.mahjong查表计算key；

---
+++ leetcode/mahjong/calcKey_test.go
@@ -0,0 +1,30 @@
+package mahjong
+
+import "testing"
+
+func Test_calcKey(t *testing.T) {
+	type args struct {
+		index []int16
+		pos   []int16
+	}
+	var pos [14]int16
+	tests := []struct {
+		name string
+		args args
+		want int32
+	}{
+		{"Test_calcKey_01", args{[]int16{
+			1, 1, 1, 0, 2, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0,
+		}, pos[0:]}, 0x0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := calcKey(tt.args.index, tt.args.pos); got != tt.want {
+				t.Errorf("calcKey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
