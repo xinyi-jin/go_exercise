6aedae7500c4389805fd02fd75f4c0f4fdc6138c
jinxinyi <1747402195.com>
2023-04-13 16:47:53 +0800

    ✨feat:add 206

---
+++ leetcode/alg206/reverseList.go
@@ -0,0 +1,30 @@
+package alg206
+
+//  Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 迭代
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+	return prev
+}
+
+func reverseListEx(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
