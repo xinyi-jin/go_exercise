baff3328863f9241c4ebe8b8fa8f358df1782f51
jinxinyi <1747402195.com>
2023-06-20 10:09:44 +0800

    ✨feat:add 26

---
+++ leetcode/alg26/removeDuplicates_test.go
@@ -0,0 +1,24 @@
+package alg26
+
+import "testing"
+
+func Test_removeDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test_removeDuplicates_01", args{[]int{1, 1, 2}}, 2},
+		{"Test_removeDuplicates_02", args{[]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeDuplicates(tt.args.nums); got != tt.want {
+				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
