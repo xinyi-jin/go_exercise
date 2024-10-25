7961e59d41c8a388b869bc96461fe1c222344980
jinxinyi <1747402195.com>
2024-10-25 18:26:02 +0800

    ✨feat:add test

--- test/all_test.go
+++ test/all_test.go
@@ -3,7 +3,9 @@ package test
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"testing"
+	"time"
 )
 
 type Reflect struct {
@@ -30,15 +32,47 @@ func TestPKGReflect(t *testing.T) {
 // 自定义测试
 func TestCustom(t *testing.T) {
 	// 测试switch 分支匹配后，还会不会继续匹配
-	var cd = 30
-	switch {
-	case cd < 30:
-		fmt.Println("cd<30")
-	case cd < 60:
-		fmt.Println("cd<60")
-		return // 测试switch 分支匹配后，return后，还会不会执行外层逻辑
-	default:
-		fmt.Println("cd<???")
+	/* {
+		var cd = 30
+		switch {
+		case cd < 30:
+			fmt.Println("cd<30")
+		case cd < 60:
+			fmt.Println("cd<60")
+			return // 测试switch 分支匹配后，return后，还会不会执行外层逻辑
+		default:
+			fmt.Println("cd<???")
+		}
+		fmt.Println("ending....")
+	} */
+
+	// 测试时间戳 （根据秒级时间戳，计算当日点时刻）
+	{
+		ts := time.Now().Unix()
+		regTime := time.Unix(ts, 0).Local()
+		regTime = time.Date(regTime.Year(), regTime.Month(), regTime.Day()+1, 0, 0, 0, int(0), regTime.Location())
+
+		fmt.Println("zore time:", regTime.Format("2006-01-02 15:04:05"))
 	}
-	fmt.Println("ending....")
+
+	// 身份证号计算对应的出生年和性别
+	{
+		idNumber := "410225199705151518"
+		var tmp string
+		for i := 6; i < 10; i++ {
+			tmp += string(idNumber[i])
+		}
+		println("tmp", tmp)
+		birthYear, err := strconv.Atoi(tmp)
+		if err != nil {
+			fmt.Errorf("Atoi err %v", err)
+		}
+		sex, err := strconv.Atoi(string(idNumber[16]))
+		if err != nil {
+			fmt.Errorf("Atoi err %v", err)
+		}
+		println("birthYear", fmt.Sprintf("%d", birthYear), "sex", sex)
+
+		println(time.Unix(0, 0).Format("2006-01-02 15:04:05"))
+	}
 }
