1a9bac2e681e470e6b4cf09ae0ced2a09afada68
jinxinyi <1747402195.com>
2024-10-25 18:25:30 +0800

    ✨feat:add 2073. 买票需要的时间

---
+++ leetcode/alg2073/timeRequiredToBuy_test.go
@@ -0,0 +1,25 @@
+package alg2073
+
+import "testing"
+
+func Test_timeRequiredToBuy(t *testing.T) {
+	type args struct {
+		tickets []int
+		k       int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Test_timeRequiredToBuy_01", args{[]int{2, 3, 2}, 2}, 6},
+		{"Test_timeRequiredToBuy_02", args{[]int{5, 1, 1, 1}, 0}, 8},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := timeRequiredToBuy(tt.args.tickets, tt.args.k); got != tt.want {
+				t.Errorf("timeRequiredToBuy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
